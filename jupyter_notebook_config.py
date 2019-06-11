2b13040edce80103746af77f3aa4e03b4eb66bb8
JarnoRFB <ruedigerbusche@web.de>
2019-06-11 23:41:38 +0200

    Remove wrong notes about area and add reference

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,5 +1,5 @@
 # Set options for solutions.
 c.JupyterLabRmotrSolutions.is_enabled = True
-c.JupyterLabRmotrSolutions.role = "teacher"
+c.JupyterLabRmotrSolutions.role = "student"
 # Enable compression, which makes sense on binder.
 c.NotebookApp.websocket_compression_options = {} 
