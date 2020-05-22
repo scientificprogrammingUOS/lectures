c6341284c3adface90e4bfcec5e2f9815b0bed2a
Chris Stenkamp <cstenkamp@uos.de>
2020-05-22 16:34:21 +0200

    update windows debugging-postbuild

--- postbuild_debugging_windows.bat
+++ postbuild_debugging_windows.bat
@@ -1,15 +1,15 @@
-conda install nodejs -y
-conda install -c conda-forge xeus-python=0.7.1 ptvsd jupyterlab=2 -y
-jupyter labextension update --all
-conda upgrade jupyterlab -y
-jupyter labextension install @jupyterlab/debugger
+CALL conda install nodejs -y
+CALL conda install -c conda-forge xeus-python=0.7.1 ptvsd -y
+CALL conda install -c conda-forge jupyterlab=2 -y
+CALL jupyter labextension update --all
+CALL jupyter labextension install @jupyterlab/debugger
 
 :: https://github.com/jupyterlab/jupyter-renderers#looking-for-plotly-extension
 :: now our old plotly-version would break, so...
-jupyter labextension uninstall @jupyterlab/plotly-extension
-conda install -c plotly plotly==4.7.1 -y
-conda install ipywidgets=7.5 -y
-jupyter labextension install jupyterlab-plotly@4.7.1
-jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.7.1
+CALL jupyter labextension uninstall @jupyterlab/plotly-extension
+CALL conda install -c plotly plotly==4.7.1 -y
+CALL conda install ipywidgets=7.5 -y
+CALL jupyter labextension install jupyterlab-plotly@4.7.1
+CALL jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.7.1
 
-jupyter lab build 
+CALL jupyter lab build 
